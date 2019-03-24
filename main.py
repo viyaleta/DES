2c7e8a7c30e1989a6b47a463853b2553261c0f1f
Viyaleta Peterson <vpeterson@indeed.com>
2019-03-24 10:11:03 -0400

    Fixing duration in timing wheel

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 
 # create an object, states, and add a state state_queue
 
+
+
 car = Object('Car', 'idle', 1)
 car.future_events = [
     ('traveling', 4),
@@ -11,24 +13,24 @@ car.future_events = [
     ('arriving', 1)
 ]
 
-bus = Object('Bus', 'departing', 10)
-bus.future_events = [
-    ('traveling', 4),
-    ('at bus stop', 1),
-    ('traveling', 3),
-    ('at bus stop', 1),
-    ('traveling', 5),
-    ('at bus stop', 3),
-    ('traveling', 1),
-    ('at bus stop', 5)
-]
+# bus = Object('Bus', 'departing', 10)
+# bus.future_events = [
+#     ('traveling', 4),
+#     ('at bus stop', 1),
+#     ('traveling', 3),
+#     ('at bus stop', 1),
+#     ('traveling', 5),
+#     ('at bus stop', 3),
+#     ('traveling', 1),
+#     ('at bus stop', 5)
+# ]
+#
+# human = Object('Human', 'walking', 2)
+# human_states = ['walking', 'standing', 'running', 'do push ups']
+# for i in range(10):
+#     random_state = random.choice(human_states)
+#     random_state_duration = random.randint(1,10)
+#     human.future_events.append((random_state, random_state_duration))
 
-human = Object('Human', 'walking', 2)
-human_states = ['walking', 'standing', 'running', 'do push ups']
-for i in range(10):
-    random_state = random.choice(human_states)
-    random_state_duration = random.randint(1,10)
-    human.future_events.append((random_state, random_state_duration))
-
-tw = TimerWheel([car, bus, human], 90)
+tw = TimerWheel([car], 90)
 tw.simulate()
--- timing_wheel.py
+++ timing_wheel.py
@@ -6,15 +6,20 @@ class Object:
         self.future_events = [] # tuple pairs of [state, duration]
 
     def update_state(self):
+        # if nothing scheduled for the future, return -1
+        if len(self.future_events) == 0:
+            return 0
+
         # get state out of future events and set it as current state
         # also pop it out of future events
         new_state, new_state_duration = self.future_events.pop(0)
         self.current_state = new_state
         self.current_state_duration = new_state_duration
-        print('{} state changed to {}'
-            .format(self.name, self.current_state))
+        print('{} state changed to {} for {} ticks'
+              .format(self.name, self.current_state, self.current_state_duration))
+        return self.current_state_duration
 
-    def schedule_next_event(self):
+    def get_next_event(self):
         if len(self.future_events)>0:
             return self.future_events[0][1]
         else:
@@ -24,14 +29,18 @@ class Object:
 class TimerWheel:
     def __init__(self, objects=[], ticks=60):
         self.ticks = ticks
+        self.wheel_number = 0
         self.current_time = -1
         self.objects = objects
         self.slots = {i:[] for i in range(ticks)}
         self.__initialize_wheel()
 
     def __initialize_wheel(self):
+        # set time to 1 = begin
         self.__tick_tock()
+
         for object in self.objects:
+            # add object to slot of "current time + duration of current state of this object"
             self.slots[self.current_time+object.current_state_duration].append(object)
             print('{} - {} added to time wheel'.format(object.name, object.current_state))
             self.__process_queue()
@@ -43,22 +52,28 @@ class TimerWheel:
 
     def __tick_tock(self):
         self.current_time += 1
-        print 'Time:', self.current_time
+        if len(self.slots[self.current_time])>0:
+            print('Time:', self.current_time)
 
     def __process_queue(self):
         ''' 1. Update state
             2. Schedule future events
         '''
 
+        # update state for each object
         for object in self.slots[self.current_time]:
             # update state
-            object.update_state()
+            current_state_duration = object.update_state()
 
+        # schedule future event for each object
         for object in self.slots[self.current_time]:
-            # schedule future event
-            next_event_time = object.schedule_next_event()
-            if next_event_time > 0:
-                self.slots[self.current_time+next_event_time].append(object)
+
+            # calculate next event time = now + how long current state will last
+            next_event_time = self.current_time + current_state_duration
+
+            # add object to the slot of next event time or complete journey
+            if next_event_time > self.current_time:
+                self.slots[next_event_time].append(object)
             else:
                 print('Simulation for {} complete'.format(object.name))
 
